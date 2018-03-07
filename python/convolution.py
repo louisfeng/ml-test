42336cc037bd214943a0a9a1b159ae9c1a209502
Louis Feng <louis.feng@intel.com>
2018-03-07 14:18:47 -0800

    added convolution test.

--- python/convolution.py
+++ python/convolution.py
@@ -10,7 +10,7 @@ num_filter = 1
 num_group = 1
 kernel = (3,) * dim
 print("kernel: ", kernel)
-shape = (1, num_filter) + (9,) * dim
+shape = (1, num_filter) + (3,) * dim
 print("shape: ", shape)
 
 x = mx.sym.Variable('x')
@@ -20,10 +20,23 @@ y1 = mx.sym.Convolution(data=x, weight=w, bias=b, num_filter=num_filter, num_gro
 print(y1.list_arguments())
 
 exe1 = y1.simple_bind(ctx, x=shape)
-x_data = mx.nd.ones(shape)
-w_data = mx.nd.ones((1,num_filter)+kernel)
-b_data = mx.nd.ones(num_filter)
+#x_data = mx.nd.ones(shape)
+x_data = mx.nd.random.normal(shape=shape)
+print("x: ", x_data)
+#w_data = mx.nd.ones((1,num_filter)+kernel)
+w_data = mx.nd.random.normal(shape=(1,num_filter)+kernel)
+print("w: ", w_data)
+#b_data = mx.nd.ones(num_filter)
+b_data = mx.nd.random.normal(shape=num_filter)
+print("b: ", b_data)
 print(y1.debug_str())
-R = exe1.forward(is_train=True, x = x_data, w=w_data, b=b_data)
-print(R[0].asnumpy())
-exe1.backward(exe1.outputs[0])
+fwd = exe1.forward(is_train=True, x = x_data, w=w_data, b=b_data)
+print(fwd[0].asnumpy())
+
+delta_data = exe1.outputs[0]
+print("delta_data:", delta_data)
+exe1.backward(delta_data)
+print("backprop gradient")
+print(exe1.grad_arrays[0].asnumpy())
+print(exe1.grad_arrays[1].asnumpy())
+print(exe1.grad_arrays[2].asnumpy())
